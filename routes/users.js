89903c48cc47bfa6d8ab9e9b59cf7cb0127cafa5
Fabian <fafunas@gmail.com>
2022-07-04 14:47:55 -0300

    GET-POST-PUT

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const {Router} = require('express');
+const {check} = require('express-validator');
+
+
+const {usersPut,usersDelete,usersGet,usersPatch,usersPost} = require('../controllers/users')
+const {validarCampos} = require('../middlewares/validateFields')
+
+
+const router = Router();
+
+router.get('/',usersGet);
+
+
+//Con el check validamos, primer argumento es el campo del body a validar, segundo argumento el mensaje
+router.post('/',[
+    check('email','El correo no es valido').isEmail(),
+    check('name','El correo no es valido').not().isEmpty(),
+    validarCampos
+] ,usersPost);
+ 
+
+router.put('/:id',[
+    check('id','Not valid ID').isMongoId(),
+    validarCampos
+], usersPut)
+
+
+module.exports = router;
